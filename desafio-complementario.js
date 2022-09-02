ed4766537b06f200381d60c88d462964dfd22426
Leonardo <leolbarrios20@gmail.com>
2022-09-02 13:54:59 -0300

    Primer commit de primera entrega del proyecto final

--- desafio-complementario.js
+++ desafio-complementario.js
@@ -1,27 +1,29 @@
+
+alert("Para continuar, presiona aceptar");
+
+let nombreCliente = prompt("Ingrese su nombre");
+
+function saludar() {
+
+    alert("Bienvenido " + nombreCliente + "! Simulá tu préstamo personal, ingresando un importe entre $10.000 y $200.000. Elegí las cuotas a pagar, entre 12, 24 y 36.");
+
+  }
+
+  while(nombreCliente == ""){
+    alert(`Debe agregar un nombre!`);
+    nombreCliente = prompt("Ingrese su nombre");
+  }
+
+  saludar();
+  
 let prestamoCliente = prompt("Cuanto dinero necesitás? (ingresá el monto sin puntos)");
 
-if(prestamoCliente <= 200000 & prestamoCliente >= 10000){
-    cuotasPrestamo = prompt("Elegí en cuantas cuotas querés abonar, podes elegir entre 12, 24 y 36");
-}else{
-    alert("Probá con otro importe, recordá que podés pedir entre $10.000 y $200.000");
+while(prestamoCliente > 200000 || prestamoCliente < 10000){
+    alert("No es importe válido. Recordá que podes pedir un préstamo desde $10.000 hasta $200.000");
+    prestamoCliente = prompt("Cuanto dinero necesitás? (ingresá el monto sin puntos)");
 }
 
-while(prestamoCliente < 10000 && prestamoCliente > 200000){
-    alert("Recordá que podés pedir entre $10.000 y $200.000");
-    prestamoCliente = prompt("Elegí entre $10.000 y $200.000");
-}
-
-if (cuotasPrestamo == 12){
-    alert("Terminás abonando por mes $" + prestamoCliente / 12 * 1.35);
-}
-
-if (cuotasPrestamo == 24){
-    alert("Terminás abonando por mes $" + prestamoCliente / 12 * 1.45);
-}
-
-if (cuotasPrestamo == 36){
-    alert("Terminás abonando por mes $" + prestamoCliente / 12 * 1.60);
-}
+let cuotasPrestamo = prompt("En cuantas cuotas querés abonar tu préstamo? Elegi entre 12, 24 y 36 cuotas.");
 
 
 while(cuotasPrestamo != 12 && cuotasPrestamo != 24 && cuotasPrestamo != 36){
@@ -30,17 +32,12 @@ while(cuotasPrestamo != 12 && cuotasPrestamo != 24 && cuotasPrestamo != 36){
 }
 
 if (cuotasPrestamo == 12){
-    alert("Terminás abonando por mes $" + prestamoCliente / 12 * 1.35);
+    alert("Terminás abonando por mes $" + (prestamoCliente / 12 * 1.35).toFixed(2));
+}else if (cuotasPrestamo == 24){
+  alert("Terminás abonando por mes $" + (prestamoCliente / 24 * 1.55).toFixed(2));
+}else if (cuotasPrestamo == 36){
+  alert("Terminás abonando por mes $" + (prestamoCliente / 36 * 1.70).toFixed(2));
 }
-
-if (cuotasPrestamo == 24){
-    alert("Terminás abonando por mes $" + prestamoCliente / 12 * 1.45);
-}
-
-if (cuotasPrestamo == 36){
-    alert("Terminás abonando por mes $" + prestamoCliente / 12 * 1.60);
-}
-
 
 
 
