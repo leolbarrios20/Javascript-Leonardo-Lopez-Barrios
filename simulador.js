3c409af4e29c642494285e68f293ef869e6d36f0
leolbarrios20 <leolbarrios20@gmail.com>
2022-09-21 21:33:59 -0300

    Se reemplazan alerts y prompts por form e inner.html

--- simulador.js
+++ simulador.js
@@ -2,130 +2,107 @@ const parrafo1 = document.getElementById("parrafo1");
 const parrafo2 = document.getElementById("parrafo2");
 const parrafo3 = document.getElementById("parrafo3");
 const parrafo4 = document.getElementById("parrafo4");
+const parrafo5 = document.getElementById("parrafo5");
+
 const tituloPrincipal = document.getElementById("tituloPrincipal");
 const tituloSecundario = document.getElementById("tituloSecundario");
+
+const formNombre = document.getElementById("formNombre");
+const formApellido = document.getElementById("formApellido");
+const formEdad = document.getElementById("formEdad");
+const formCuotas = document.getElementById("formCuotas");
+const formPrestamo = document.getElementById("formPrestamo");
+
+const nombreForm = document.getElementById("nombreForm");
+const apellidoForm = document.getElementById("apellidoForm");
+const edadForm = document.getElementById("edadForm");
+const cuotasForm = document.getElementById("cuotasForm");
+const prestamoForm = document.getElementById("prestamoForm");
 
 const importeMinimo = 10000;
 const importeMaximo = 200000;
 
-let nombreCliente = prompt("Ingresá tu nombre");
-
-let bienvenidaCliente = `Bienvenido/a ${nombreCliente}! Simulá tu préstamo personal, ingresando un importe entre $${importeMinimo} y $${importeMaximo}. Elegí las cuotas a pagar, entre 12, 24 y 36.`;
-let edadNoValida = `Lamentamos informarte ${nombreCliente}, que debés tener al menos 18 años para solicitar tu préstamo personal.`;
-
-function saludar() {
-  alert(bienvenidaCliente);
-}
-
-while(nombreCliente == ""){
-  alert("Debés ingresar un nombre.");
-  nombreCliente = prompt("Ingresá tu nombre");
-}
-
-let edadCliente = prompt("Ingresá tu edad");
-
-while(edadCliente < 18){
-  alert(edadNoValida);
-  edadCliente = prompt("Ingresá tu edad");
-}
-
-while(edadCliente > 79){
-  alert(edadNoValida);
-  edadCliente = prompt("Ingresá tu edad");
-}
-
-  saludar();         //Cuando se cumplan todas las condiciones anteriores, se ejecutará la función "saludar".
-  
-let prestamoCliente = prompt("Cuanto dinero necesitás? (ingresá el monto sin puntos)");
-
-while(prestamoCliente > importeMaximo || prestamoCliente < importeMinimo){           
-    alert("No es un importe válido. Recordá que podés pedir un préstamo desde $10.000 hasta $200.000");
-    prestamoCliente = prompt("Cuanto dinero necesitás? (ingresá el monto sin puntos)");
-}
-
-let arrayCliente = [nombreCliente, edadCliente, prestamoCliente];
-alert("Estás por simular un préstamo por $" + arrayCliente[2]);
-
-let cuotasPrestamo = prompt("En cuantas cuotas querés abonar tu préstamo? Elegi entre 12, 24 y 36 cuotas.");
-
-while(cuotasPrestamo != 12 && cuotasPrestamo != 24 && cuotasPrestamo != 36){
-    alert("Recordá que las cuotas pueden ser entre 12, 24 y 36");
-    cuotasPrestamo = prompt("Elegí entre 12, 24 y 36");
-}
-
-const propiedadesCliente = {
-  nombre: nombreCliente,
-  edad: edadCliente,
-  prestamoSolicitado: prestamoCliente,
-  cuotasSolicitadas: cuotasPrestamo
-};
-
-arrayCliente.push(cuotasPrestamo);
-
-if(cuotasPrestamo == 12){
-  alert("Terminás abonando por mes $" + (prestamoCliente / cuotasPrestamo * 1.35).toFixed(2));
-}else if (cuotasPrestamo == 24){
-alert("Terminás abonando por mes $" + (prestamoCliente / cuotasPrestamo  * 1.50).toFixed(2));
-}else if (cuotasPrestamo == 36){
-alert("Terminás abonando por mes $" + (prestamoCliente / cuotasPrestamo  * 1.70).toFixed(2));
-}
-
-let confirmacionPrestamo = prompt("Para confirmar y mostrarte el importe e intereses de tu préstamo, ingresá 'si', de lo contrario, ingresá 'no'");
-const confirmacionTrue = "si";
-const confirmacionFalse = "no";
-
-let prestamoSolicitadoFinal12 = "Préstamo personal solicitado: $" + arrayCliente[2];
-let montoMensual12 = "Monto mensual a abonar: $" + (arrayCliente[2] / arrayCliente[3] * 1.45).toFixed(2);
-let interesesPorCuota12 = "Intereses por cuota: $" + (arrayCliente[2] / arrayCliente[3] * 1.45 - (arrayCliente[2] / arrayCliente[3])).toFixed(2);
-let interesesTotales12 = "Intereses totales: $" + ((arrayCliente[2] / arrayCliente[3] * 1.45 - (arrayCliente[2] / arrayCliente[3])) * arrayCliente[3]).toFixed(2);
-
-let prestamoSolicitadoFinal24 = "Préstamo personal solicitado: $" + arrayCliente[2];
-let montoMensual24 = "Monto mensual a abonar: $" + (arrayCliente[2] / arrayCliente[3] * 1.60).toFixed(2);
-let interesesPorCuota24 = "Intereses por cuota: $" + (arrayCliente[2] / arrayCliente[3] * 1.60 - (arrayCliente[2] / arrayCliente[3])).toFixed(2);
-let interesesTotales24 = "Intereses totales: $" + ((arrayCliente[2] / arrayCliente[3] * 1.60 - (arrayCliente[2] / arrayCliente[3])) * arrayCliente[3]).toFixed(2);
-
-let prestamoSolicitadoFinal36 = "Préstamo personal solicitado: $" + arrayCliente[2];
-let montoMensual36 = "Monto mensual a abonar: $" + (arrayCliente[2] / arrayCliente[3] * 1.80).toFixed(2);
-let interesesPorCuota36 = "Intereses por cuota: $" + (arrayCliente[2] / arrayCliente[3] * 1.80 - (arrayCliente[2] / arrayCliente[3])).toFixed(2);
-let interesesTotales36 = "Intereses totales: $" + ((arrayCliente[2] / arrayCliente[3] * 1.80 - (arrayCliente[2] / arrayCliente[3])) * arrayCliente[3]).toFixed(2);
-
-let confirmacionPrestamo12 = ("Préstamo personal solicitado: $" + arrayCliente[2] + ". Monto mensual a abonar: $" + (arrayCliente[2] / arrayCliente[3] * 1.45).toFixed(2) + ". Intereses por cuota: $" + (arrayCliente[2] / arrayCliente[3] * 1.45 - (arrayCliente[2] / arrayCliente[3])).toFixed(2) + ". Intereses totales: $" + ((arrayCliente[2] / arrayCliente[3] * 1.45 - (arrayCliente[2] / arrayCliente[3])) * arrayCliente[3]).toFixed(2));
-let confirmacionPrestamo24 = ("Préstamo personal solicitado: $" + arrayCliente[2] + ". Monto mensual a abonar: $" + (arrayCliente[2] / arrayCliente[3] * 1.60).toFixed(2) + ". Intereses por cuota: $" + (arrayCliente[2] / arrayCliente[3] * 1.60 - (arrayCliente[2] / arrayCliente[3])).toFixed(2) + ". Intereses totales: $" + ((arrayCliente[2] / arrayCliente[3] * 1.60 - (arrayCliente[2] / arrayCliente[3])) * arrayCliente[3]).toFixed(2));
-let confirmacionPrestamo36 = ("Préstamo personal solicitado: $" + arrayCliente[2] + ". Monto mensual a abonar: $" + (arrayCliente[2] / arrayCliente[3] * 1.80).toFixed(2) + ". Intereses por cuota: $" + (arrayCliente[2] / arrayCliente[3] * 1.80 - (arrayCliente[2] / arrayCliente[3])).toFixed(2) + ". Intereses totales: $" + ((arrayCliente[2] / arrayCliente[3] * 1.80 - (arrayCliente[2] / arrayCliente[3])) * arrayCliente[3]).toFixed(2));
-
-for(let i = 0; i < 1; i++){
-  if(cuotasPrestamo == 12 && confirmacionPrestamo == "si"){
-    alert("Felicitaciones! A continuación te mostraremos el detalle de tu préstamo personal.");
-    parrafo1.innerHTML = (prestamoSolicitadoFinal12);
-    parrafo2.innerHTML = (montoMensual12);
-    parrafo3.innerHTML = (interesesPorCuota12);
-    parrafo4.innerHTML = (interesesTotales12);
-    tituloPrincipal.innerHTML = ("Gracias" + nombreCliente);
-    tituloSecundario.innerHTML = `Detalle de tu préstamo personal`;
-  }else if(cuotasPrestamo == 24 && confirmacionPrestamo == "si"){
-    alert("Felicitaciones! A continuación te mostraremos el detalle de tu préstamo personal.");
-    parrafo1.innerHTML = (prestamoSolicitadoFinal24);
-    parrafo2.innerHTML = (montoMensual24);
-    parrafo3.innerHTML = (interesesPorCuota24);
-    parrafo4.innerHTML = (interesesTotales24);
-    tituloPrincipal.innerHTML = ("Gracias" + nombreCliente);
-    tituloSecundario.innerHTML = `Detalle de tu préstamo personal`;
-  }else if(cuotasPrestamo == 36 && confirmacionPrestamo == "si"){
-    alert("Felicitaciones! A continuación te mostraremos el detalle de tu préstamo personal.");
-    parrafo1.innerHTML = (prestamoSolicitadoFinal36);
-    parrafo2.innerHTML = (montoMensual36);
-    parrafo3.innerHTML = (interesesPorCuota36);
-    parrafo4.innerHTML = (interesesTotales36);
-    tituloPrincipal.innerHTML = ("Gracias" + nombreCliente);
-    tituloSecundario.innerHTML = `Detalle de tu préstamo personal`;
-  }else if(confirmacionPrestamo === confirmacionFalse){
-    alert(`Lamentamos que no quieras continuar, si cambiás de opinión, tus $${prestamoCliente} te esperan!`);
-    tituloPrincipal.innerHTML = ("Volvé pronto " + nombreCliente + "!");
-    break;
-}else{
-  confirmacionPrestamo = prompt(confirmacionPrestamo + " No es un dato válido, intentá de nuevo.");
+class Cliente {
+  constructor(nombre, apellido, edad, cuotas, prestamo) {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+      this.cuotas = cuotas;
+      this.prestamo = prestamo;
   }
 }
 
-const buscado = arrayCliente.filter(cliente => propiedadesCliente.edad > 18);
+const miFormulario = document.getElementById("miFormulario");
+
+miFormulario.addEventListener("submit", (e) =>{
+    e.preventDefault();
+
+    const nombre = document.getElementById("formNombre");
+
+    if(nombre.value == ""){
+      nombreForm.innerHTML = ("Debés ingresar un nombre.");
+      miFormulario.reset();
+      HTMLFormElement.reset();
+    }
+
+    const apellido = document.getElementById("formApellido");
+
+    if(apellido.value == ""){
+      apellidoForm.innerHTML = ("Debés ingresar un apellido.");
+    }
+
+    const edad = document.getElementById("formEdad");
+
+    if(edad.value < 18){
+      edadForm.innerHTML = ("Debes tener mas de 18 años para solicitar el prestamo");
+    }else if(edad.value > 79){
+      edadForm.innerHTML = ("Debes tener menos de 80 años para solicitar el prestamo");
+    }
+
+    const prestamo = document.getElementById("formPrestamo");
+
+    if(prestamo.value > importeMaximo || prestamo.value < importeMinimo){           
+      prestamoForm.innerHTML = ("No es un importe válido. Recordá que podés pedir un préstamo desde $10.000 hasta $200.000"); 
+  }
+
+    const cuotas = document.getElementById("formCuotas");
+
+    for(let i = 0; i < 1; i++){
+      if(cuotas.value == 12){
+        parrafo1.innerHTML = ("Préstamo personal solicitado: $" + prestamo.value);
+        parrafo2.innerHTML = ("Monto mensual a abonar: $" + (prestamo.value / cuotas.value * 1.45).toFixed(2));
+        parrafo3.innerHTML = ("Intereses por cuota: $" + (prestamo.value / cuotas.value * 1.45 - (prestamo.value / cuotas.value)).toFixed(2));
+        parrafo4.innerHTML = ("Intereses totales: $" + ((prestamo.value / cuotas.value * 1.45 - (prestamo.value / cuotas.value)) * cuotas.value).toFixed(2));
+        parrafo5.innerHTML = ("Cuotas a pagar: " + cuotas.value);
+        tituloPrincipal.innerHTML = (`Gracias ${nombre.value} ${apellido.value}!`);
+        tituloSecundario.innerHTML = `Este es el detalle de tu préstamo personal`;
+      }else if(cuotas.value == 24){
+        parrafo1.innerHTML = ("Préstamo personal solicitado: $" + prestamo.value);
+        parrafo2.innerHTML = ("Monto mensual a abonar: $" + (prestamo.value / cuotas.value * 1.60).toFixed(2));
+        parrafo3.innerHTML = ("Intereses por cuota: $" + (prestamo.value / cuotas.value * 1.60 - (prestamo.value / cuotas.value)).toFixed(2));
+        parrafo4.innerHTML = ("Intereses totales: $" + ((prestamo.value / cuotas.value * 1.60 - (prestamo.value / cuotas.value)) * cuotas.value).toFixed(2));
+        parrafo5.innerHTML = ("Cuotas a pagar: " + cuotas.value);
+        tituloPrincipal.innerHTML = ("Gracias " + cliente.value + "!");
+        tituloSecundario.innerHTML = `Este es el detalle de tu préstamo personal`;
+      }else if(cuotas.value == 36){
+        parrafo1.innerHTML = ("Préstamo personal solicitado: $" + prestamo.value);
+        parrafo2.innerHTML = ("Monto mensual a abonar: $" + (prestamo.value / cuotas.value * 1.80).toFixed(2));
+        parrafo3.innerHTML = ("Intereses por cuota: $" + (prestamo.value / cuotas.value * 1.80 - (prestamo.value / cuotas.value)).toFixed(2));
+        parrafo4.innerHTML = ("Intereses totales: $" + ((prestamo.value / cuotas.value * 1.80 - (prestamo.value / cuotas.value)) * cuotas.value).toFixed(2));
+        parrafo5.innerHTML = ("Cuotas a pagar: " + cuotas.value);
+        tituloPrincipal.innerHTML = ("Gracias " + cliente.value + "!");
+        tituloSecundario.innerHTML = `Este es el detalle de tu préstamo personal`;
+      }else{
+        cuotasForm.innerHTML = ("Recordá que las cuotas pueden ser 12, 24 o 36");
+      }
+    }
+    
+    const cliente = new Cliente(nombre.value, apellido.value, edad.value, cuotas.value, prestamo.value);
+    console.log(Cliente);
+    miFormulario.reset();
+});
+
+
+
+
 
